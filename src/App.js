1fabd6435e512db4c2c6551c70efd99c903eb43a
Cynthia Eburu <cyeburu@yahoo.com>
2020-06-04 01:58:56 +0100

    SearchBtn updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import SearchButton from "./SearchButton"
 import './App.css';
-import ListOfBabyNames from './ListOfBabyNames'
-import BabyNamesData from "./BabyNamesData.json"
 
 function App() {
-  const [text, setText] = useState("");
-
-  const filteredNames = BabyNamesData.filter(baby => baby.name.toLowerCase().includes(text.toLowerCase()));
   return (
     <div className="nameContainer">
       <header className="App-header">
-        <input
-          value={text}
-          className="searchbtn"
-          type="search"
-          placeholder="Search for a name..."
-          onChange={e => setText(e.target.value)}
-        />
-        {/* <SearchButton /> */}
-        <ListOfBabyNames names={filteredNames} />
+        <SearchButton />
       </header>
     </div>
   );
--- src/SearchButton.js
+++ src/SearchButton.js
@@ -1,17 +1,21 @@
-import React from "react"
+import React, { useState } from "react";
+import BabyNamesData from "./BabyNamesData.json";
+import ListOfBabyNames from "./ListOfBabyNames"
 
 const SearchButton = () => {
-    function searchInput(event) {
-        let userInput = event.target.value
+    const [text, setText] = useState("");
+    const filteredNames = BabyNamesData.filter(baby => baby.name.toLowerCase().includes(text.toLowerCase()));
 
-    }
-    return (
-        <div>
-            <input onClick={searchInput} className="searchbtn" type="search" placeholder="Search for a name..."></input>
-        </div>
-
+    return (<div><input
+        value={text}
+        className="searchbtn"
+        type="search"
+        placeholder="Search for a name..."
+        onChange={userInput => setText(userInput.target.value)}
+    />
+        <ListOfBabyNames names={filteredNames} />
+    </div>
     )
-
 }
 
 export default SearchButton
